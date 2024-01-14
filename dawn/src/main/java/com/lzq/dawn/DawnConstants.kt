30859cfc857c2cf9e42594af75307d77203a9717
周小强 <642744460@qq.com>
2024-01-14 14:13:57 +0800

    update: 工具类改成kt实现

--- dawn/src/main/java/com/lzq/dawn/DawnConstants.kt
+++ dawn/src/main/java/com/lzq/dawn/DawnConstants.kt
@@ -1,9 +1,6 @@
-package com.lzq.dawn;
+package com.lzq.dawn
 
-import androidx.annotation.IntDef;
-
-import java.lang.annotation.Retention;
-import java.lang.annotation.RetentionPolicy;
+import androidx.annotation.IntDef
 
 /**
  * @Name :DawnConstants
@@ -11,31 +8,27 @@ import java.lang.annotation.RetentionPolicy;
  * @Author :  Lzq
  * @Desc :
  */
-public class DawnConstants {
-    public static final class MemoryConstants {
+class DawnConstants {
+    object MemoryConstants {
+        const val BYTE = 1
+        const val KB = 1024
+        const val MB = 1048576
+        const val GB = 1073741824
 
-        public static final int BYTE = 1;
-        public static final int KB = 1024;
-        public static final int MB = 1048576;
-        public static final int GB = 1073741824;
-
-        @IntDef({BYTE, KB, MB, GB})
-        @Retention(RetentionPolicy.SOURCE)
-        public @interface Unit {
-        }
+        @IntDef(BYTE, KB, MB, GB)
+        @Retention(AnnotationRetention.SOURCE)
+        annotation class Unit
     }
 
-    public static final class TimeConstants {
+    object TimeConstants {
+        const val MSEC = 1
+        const val SEC = 1000
+        const val MIN = 60000
+        const val HOUR = 3600000
+        const val DAY = 86400000
 
-        public static final int MSEC = 1;
-        public static final int SEC = 1000;
-        public static final int MIN = 60000;
-        public static final int HOUR = 3600000;
-        public static final int DAY = 86400000;
-
-        @IntDef({MSEC, SEC, MIN, HOUR, DAY})
-        @Retention(RetentionPolicy.SOURCE)
-        public @interface Unit {
-        }
+        @IntDef(MSEC, SEC, MIN, HOUR, DAY)
+        @Retention(AnnotationRetention.SOURCE)
+        annotation class Unit
     }
 }
