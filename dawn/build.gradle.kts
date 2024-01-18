cf4fad21f9d6998f4afac2cd9511632969b9ac6c
周小强 <642744460@qq.com>
2024-01-18 16:01:02 +0800

    chore:jitpack仓库参数配置转移到gradle.properties文件中

--- dawn/build.gradle.kts
+++ dawn/build.gradle.kts
@@ -167,6 +167,17 @@ if (secretPropsFile.exists()) {
     println("No props file, loading env vars")
 }
 
+val packagePropsFile = project.rootProject.file("gradle.properties")
+if (packagePropsFile.exists()) {
+    val p =  Properties()
+    p.load(FileInputStream(packagePropsFile))
+    p.forEach { name, value ->
+        extra[name.toString()] = value
+    }
+} else {
+    println("No props file, loading env vars")
+}
+
 var publishVersion    = extra["PUBLISH_VERSION"].toString()
 var mavenGroupId      = extra["PUBLISH_GROUP_ID"].toString()
 var mavenArtifactId   = extra["PUBLISH_ARTIFACT_ID"].toString()
